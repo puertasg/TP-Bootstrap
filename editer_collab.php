066e53ad380bdb9f93a2bf7c206328885146765e
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-17 15:40:14 +0200

    Ajout img-fluid pour les images des cards, wrap du text dans des h4 pour editer_collab

--- editer_collab.php
+++ editer_collab.php
@@ -10,10 +10,29 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
     crossorigin="anonymous">
 
-  <title>Editer un collaborateur</title>
+  <link rel="stylesheet" href="./css/style.css">
+
+  <title>Edition collaborateur</title>
 </head>
 
 <body>
+  <?php include "elements/navbar.html"; ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <img src="./images/default-profile.png" class="img-fluid" alt="photo"/>
+      </div>
+
+      <div class="col-8">
+        <h4 class="text-uppercase">Nom</h4>
+        <h4 class="text-capitalize">Prénom</h4>
+        -
+        <span>Matricule</span>
+        <input type="checkbox">Désactiver
+      </div>
+    </div>
+    
+  </div>
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
